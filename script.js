8b58e7100fd62f79792e545e8e9be70ec63721c3
test <test@Jonathans-MacBook.local>
2017-12-17 13:34:46 -0800

    adding the commits

---
+++ script.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+console.log("Sanity Check");
+
+
+
+
+$(function() {
+    $('body').vegas({
+        slides: [
+            { src: 'https://i.imgur.com/pRWZR0X.jpg' },
+            { src: 'https://i.imgur.com/AcR0hqH.jpg' },
+            { src: 'https://i.imgur.com/CmVVsxL.jpg' }
+        ]
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+});
